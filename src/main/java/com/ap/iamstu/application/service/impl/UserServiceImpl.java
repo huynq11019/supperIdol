129a6c9dbd541146626e71b95889fedacba992e7
Nguyen Quang Huy <vosong9201@gmail.com>
2022-01-11 14:36:59 +0700

    update template

--- src/main/java/com/ap/iamstu/application/service/impl/UserServiceImpl.java
+++ src/main/java/com/ap/iamstu/application/service/impl/UserServiceImpl.java
@@ -6,8 +6,12 @@ import com.ap.iamstu.application.service.UserService;
 import com.ap.iamstu.domain.User;
 import com.ap.iamstu.infrastructure.persistence.entity.UserEntity;
 import com.ap.iamstu.infrastructure.support.Service.AbstractDomainService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class UserServiceImpl extends AbstractDomainService<User, UserEntity, String> implements UserService{
 
     protected UserServiceImpl(JpaRepository<UserEntity, String> jpaRepository, EntityMapper<User, UserEntity> mapper) {
