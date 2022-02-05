4ed812854b6b6aa48d69cf90fd2b071e8fd759cf
Nguyen Quang Huy <vosong9201@gmail.com>
2022-02-05 23:43:42 +0700

    update handler exception

--- src/main/java/com/ap/iamstu/IamstuApplication.java
+++ src/main/java/com/ap/iamstu/IamstuApplication.java
@@ -14,6 +14,27 @@ public class IamstuApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IamstuApplication.class, args);
+
+        System.out.println("" +
+                "\n" +
+                "         -`+yMMMMdMdNMdy.                                                                           \n" +
+                "      `oNmNMd+MN:+Mmo``                                                                             \n" +
+                "    `omMm-mM:-d/.dd-                                                                                \n" +
+                "   .hmyMy -mohhNNMMNhhh/-`                                                                          \n" +
+                "  ohMN//h+yNMMMNhhhhhhhhymo:       HIHI                                                                 \n" +
+                " :NysMh.yNMMMhys.````````..`                                                                        \n" +
+                ".hMNs/:hMMhdMdNMmo.`               `-//                                       `...`                 \n" +
+                ":MhmNhhMMhmNo.:+MMmdyo:-        `:+mNMm\n" +
+                "-NMdo-sMydNo`  .MMMMMdNh     .-shMMMMMM\n" +
+                "-NMmdsyN/Md    .mNMNh`dm+++ydmMMMMMMMMh\n" +
+                ":NMy+-yN/MN+    `-oo+sMMMMMMMMMMMMMMd+`\n" +
+                "/mMmmhysoMMNy-````dMMMMMMMMNNNNNdds/`  \n" +
+                "NNmyyydmoomMMNddddmMNNNMMMMdhh+:`      \n" +
+                "::`       `+yNMMNys+:::ssyNNMMMNo.     \n" +
+                "             :my`         ``/ohNMd     \n" +
+                "            `hm`               .:/     \n" +
+                "           `ym:                        \n" +
+                "           oMMMy       ");
     }
 
 }
